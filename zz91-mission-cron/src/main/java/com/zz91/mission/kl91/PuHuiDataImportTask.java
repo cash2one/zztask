5bab73e069ec0b6d960d40056891368138b3b5fa
伍金成 <wjcsm070918@gmail.com>
2012-07-13 19:46:01 +0800

    fixed:kl91数据导入改写方法

--- zz91-mission-cron/src/main/java/com/zz91/mission/kl91/PuHuiDataImportTask.java
+++ zz91-mission-cron/src/main/java/com/zz91/mission/kl91/PuHuiDataImportTask.java
@@ -12,6 +12,7 @@ import com.zz91.task.common.ZZTask;
 import com.zz91.util.datetime.DateUtil;
 import com.zz91.util.db.DBUtils;
 import com.zz91.util.db.IReadDataHandler;
+import com.zz91.util.db.pool.DBPoolFactory;
 
 public class PuHuiDataImportTask implements ZZTask{
 	
@@ -27,7 +28,7 @@ public class PuHuiDataImportTask implements ZZTask{
 	@Override
 	public boolean exec(Date baseDate) throws Exception {
 		final List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
-		String sql="select c.id,ca.email,ca.account,c.address,c.introduction,c.business,ca.contact,ca.sex,c.name,ca.mobile,ca.tel from company c left join company_account ca on ca.company_id=c.id " +
+		String sql="select c.id,ca.email,ca.account,c.address,c.introduction,c.business,ca.contact,ca.sex,c.name,ca.mobile,ca.tel,c.address from company c left join company_account ca on ca.company_id=c.id " +
 				"where c.membership_code= '10051000' and c.industry_code='10001000'";
 		DBUtils.select(DB_AST, sql, new IReadDataHandler() {
 			@Override
@@ -45,6 +46,7 @@ public class PuHuiDataImportTask implements ZZTask{
 					map.put("name", rs.getString(9));
 					map.put("mobile", rs.getString(10));
 					map.put("tel", rs.getString(11));
+					map.put("address", rs.getString(12));
 					list.add(map);
 				}
 			}
@@ -65,7 +67,7 @@ public class PuHuiDataImportTask implements ZZTask{
 					
 			Integer registFlag=1;
 					
-			String membershipCode="10051001";
+			String membershipCode="10051000";
 					
 			String email=(String) company.get("email");
 			if(email==null){
@@ -104,16 +106,144 @@ public class PuHuiDataImportTask implements ZZTask{
 			if(tel==null){
 				tel="";
 			}
+			String address=(String) company.get("address");
+			if(address==null){
+				address="杭州";
+			}
 					//判断是否是未过期的高会
 			String svrCode="1000";			
 			Integer data=validatePeriod(cid, svrCode);
 			if(data.intValue()>1){
 				return false;
 			}
-//			saveToKL(cid,email,account,membershipCode,business,contact,sex,name,mobile,tel,
-//				numLogin,gmtLastLogin,industryCode,domain,isActive,registFlag);					
+			saveToKL(cid,email,account,membershipCode,business,contact,sex,name,mobile,tel,
+				numLogin,gmtLastLogin,industryCode,domain,isActive,registFlag,address);					
 			}
 		return false;
+	}
+	
+	private void saveToKL(Integer cid,String email, String account, String membershipCode,
+			String business, String contact, Integer sex, String name,
+			String mobile, String tel, Integer numLogin, String gmtLastLogin,
+			String industryCode, String domain, Integer isActive,
+			Integer registFlag,String address) {
+		String sql="insert into company(account,company_name,membership_code,sex,contact,mobile,address,email,tel,business,num_login,gmt_last_login,is_active,domain,industry_code,regist_flag,show_time,gmt_created,gmt_modified)values('"+account+"','"+name+"','"+membershipCode+"',"+sex+",'"+contact+"','"+mobile+"','"+address+"','"+email+"','"+tel+"','"+business+"','"+numLogin+"','"+gmtLastLogin+"','"+isActive+"','"+domain+"','"+industryCode+"','"+registFlag+"',now(),now(),now())";
+		DBUtils.insertUpdate(DB_KL91, sql );
+		final Integer [] i = new Integer[1];
+		DBUtils.select(DB_KL91, "select last_insert_id()", new IReadDataHandler() {
+			@Override
+			public void handleRead(ResultSet rs) throws SQLException {
+				while (rs.next()) {
+					i[0] = rs.getInt(1);
+				}
+			}
+		});
+		selectProducts(cid,i[0]);
+	}
+	
+	private void selectProducts(Integer cid,Integer companyId) {
+		final List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+		String sql="select products_type_code,title,details,location,price_unit,quantity_unit,quantity,color,useful,min_price,max_price from products where company_id="+cid+" and check_status=1 and is_del=0 and is_pause=1";
+		DBUtils.select(DB_AST, sql, new IReadDataHandler() {
+			@Override
+			public void handleRead(ResultSet rs) throws SQLException {
+				while(rs.next()){
+					Map<String,Object> map = new HashMap<String, Object>();
+					map.put("productsTypeCode", rs.getString(1));
+					map.put("title", rs.getString(2));
+					map.put("details", rs.getString(3));
+					map.put("location", rs.getString(4));
+					map.put("priceUnit", rs.getString(5));
+					map.put("quantityUnit", rs.getString(6));
+					map.put("quantity", rs.getString(7));
+					map.put("color", rs.getString(8));
+					map.put("useful", rs.getString(9));
+					map.put("minPrice", rs.getFloat(10));
+					map.put("maxPrice", rs.getFloat(10));
+					list.add(map);
+				}
+			}
+		});
+		for(Map<String, Object> product:list){
+			//Integer cpid=0;
+			String productCategoryCode="废塑料";
+			String typeCode=(String)product.get("productsTypeCode");
+			if(typeCode.equals("10331001")){
+				typeCode="0";
+			}
+			if(typeCode.equals("10331000")){
+				typeCode="1";
+			}
+			String title=(String)product.get("title");
+			if(title==null){
+				title="";
+			}
+			String details=(String)product.get("details");
+			if(details==null){
+				details="";
+			}
+			String detailsQuery="";
+			Integer checkedFlag=1;
+			Integer deletedFlag=0;
+			Integer imptFlag=1;
+			Integer publishFlag=1;
+			String gmtPost= DateUtil.toString(new Date(),"yyyy-MM-dd HH:mm:ss");
+			String gmtRefresh=DateUtil.toString(new Date(),"yyyy-MM-dd HH:mm:ss");
+			String gmtExpired=DateUtil.toString(DateUtil.getDateAfterMonths(new Date(), +6), "yyyy-MM-dd HH:mm:ss");
+			String location=(String)product.get("location");
+			if(location==null){
+				location="";
+			}
+			String useful=(String)product.get("useful");
+			if(useful==null){
+				useful="";
+			}
+			String priceUnit=(String)product.get("priceUnit");
+			if(priceUnit==null){
+				priceUnit="";
+			}
+			
+			String color=(String)product.get("color");
+			if(color==null){
+				color="";
+			}
+			String quantityUnit=(String)product.get("quantityUnit");
+			if(quantityUnit==null){
+				quantityUnit="";
+			}
+			String quantitys=(String)product.get("quantity");
+			Integer quantity=0;
+			if(quantitys.equals("")){
+				quantity=0;
+			}
+			Float minPrices=(Float)product.get("minPrice");
+			Integer minPrice=0;
+			if(minPrices==null){
+				minPrice=0;
+			}
+			Float maxPrices=(Float)product.get("maxPrice");
+			Integer maxPrice=0;
+			if(maxPrices==null){
+				maxPrice=0;
+			}
+			if(list.size()>0){
+				insertProducts(companyId,productCategoryCode,typeCode,title,details,detailsQuery,checkedFlag,deletedFlag,imptFlag
+						,publishFlag,location,useful,gmtPost,gmtRefresh,gmtExpired,color,priceUnit,quantityUnit,quantity,minPrice,maxPrice);
+			}
+		}
+	}
+	
+	private void insertProducts(Integer companyId,String productCategoryCode, String typeCode,
+			String title, String details, String detailsQuery,
+			Integer checkedFlag, Integer deletedFlag, Integer imptFlag,
+			Integer publishFlag, String location, String useful,String gmtPost,String gmtRefresh,String gmtExpired,String color,
+			String priceUnit, String quantityUnit, Integer quantity,
+			Integer minPrice, Integer maxPrice) {
+		String sql="insert into products(cid,products_category_code,type_code,title,details,details_query,checked_flag," +
+				"deleted_flag,impt_flag,publish_flag,location,useful,gmt_post,gmt_refresh,gmt_expired,color,price_unit,quantity_unit,quantity,min_price,max_price,show_time,gmt_created,gmt_modified,gmt_check)" +
+				"values('"+companyId+"','"+productCategoryCode+"','"+typeCode+"','"+title+"','"+details+"','"+detailsQuery+"','"+checkedFlag+"','"+deletedFlag+"'," +
+						"'"+imptFlag+"','"+publishFlag+"','"+location+"','"+useful+"','"+gmtPost+"','"+gmtRefresh+"','"+gmtExpired+"','"+color+"','"+priceUnit+"','"+quantityUnit+"','"+quantity+"','"+minPrice+"','"+maxPrice+"',now(),now(),now(),now())";
+		DBUtils.insertUpdate(DB_KL91, sql);
 	}
 	
 	private Integer validatePeriod(Integer cid, String svrCode) {
@@ -135,6 +265,14 @@ public class PuHuiDataImportTask implements ZZTask{
 	public boolean init() throws Exception {
 		
 		return false;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		DBPoolFactory.getInstance().init(
+				"file:/usr/tools/config/db/db-zztask-jdbc.properties");
+		PuHuiDataImportTask obj = new PuHuiDataImportTask();
+		Date date = DateUtil.getDate("2012-04-20", "yyyy-MM-dd");
+		obj.exec(date);
 	}
 
 }
