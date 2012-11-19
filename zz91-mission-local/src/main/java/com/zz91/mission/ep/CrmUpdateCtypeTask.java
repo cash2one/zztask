b5a6bde181f93939cc135cb428073f55d4c7fe9a
齐振杰 <qizj@asto.com.cn>
2012-11-19 10:35:36 +0800

    增加更新关系表sale_type字段为1

--- zz91-mission-local/src/main/java/com/zz91/mission/ep/CrmUpdateCtypeTask.java
+++ zz91-mission-local/src/main/java/com/zz91/mission/ep/CrmUpdateCtypeTask.java
@@ -16,6 +16,7 @@ public class CrmUpdateCtypeTask implements ZZTask {
 
 	final static String DATE_FORMAT = "yyyy-MM-dd";
 	final static String DB="crm";
+	
 	@Override
 	public boolean clear(Date baseDate) throws Exception {
 		return false;
@@ -25,14 +26,23 @@ public class CrmUpdateCtypeTask implements ZZTask {
 	public boolean exec(Date baseDate) throws Exception {
 		String date=DateUtil.toString(baseDate, DATE_FORMAT);
 		updateCtype(date);
+		updateSaleTypeByCtype(date);
 		return true;
 	}
+	
 	public boolean updateCtype(String date){
 		StringBuffer sql=new StringBuffer("update crm_company set ctype=3,gmt_modified=now() where member_code='10011001'");
 		if(!StringUtils.isEmpty(date)){
 			sql.append(" and date_format(gmt_modified,'%Y-%m-%d')='"+date+"'");
 		}
 		
+		boolean isok=DBUtils.insertUpdate(DB, sql.toString());
+		return isok;
+	}
+	
+	public boolean updateSaleTypeByCtype(String date){
+		StringBuffer sql = new StringBuffer("update crm_sale_comp csc set csc.gmt_modified=now(),csc.sale_type=1");
+		sql.append("where csc.status=1 and csc.cid in (select id from crm_company where ctype=3 and gmt_modified='"+date+"')");
 		boolean isok=DBUtils.insertUpdate(DB, sql.toString());
 		return isok;
 	}
